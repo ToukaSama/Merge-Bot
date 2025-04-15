260c0ff003ec26d89c6fa20a63f73a541eafe5c3
ToukaSama <blacksummoner6659@gmail.com>
2025-04-15 11:26:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ from __init__ import (
     replyDB,
 )
 from config import Config
-from helpers import database
+
 from helpers.utils import UserSettings, get_readable_file_size, get_readable_time
 
 botStartTime = time.time()
