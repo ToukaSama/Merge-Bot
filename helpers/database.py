13243e628cfe1e728faa13929a8d42cd25044167
ToukaSama <blacksummoner6659@gmail.com>
2025-04-15 11:18:04 +0530

    Update database.py

--- helpers/database.py
+++ helpers/database.py
@@ -19,7 +19,7 @@ class Database(object):
         LOGGER.info(f"New user added id={uid}\n{fname} {lname} \n")
     except DuplicateKeyError:
         LOGGER.info(f"Duplicate Entry Found for id={uid}\n{fname} {lname} \n")
-    return
+    
 
 
 async def broadcast():
